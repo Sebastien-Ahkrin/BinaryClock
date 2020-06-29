4232b5dcb013e0270472b2291ba2f63913f84462
Sebastien Ahkrin <sebastien48criquet@gmail.com>
2020-06-29 14:39:55 +0200

    chore: export default component

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import './App.css'
 
-function App() {
+export default function App () {
   return (
     <div>
       <p>Hello, World</p>
     </div>
   )
 }
-
-export default App
