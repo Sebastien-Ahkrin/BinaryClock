c174de995f062e310235a09d6367601e1809d7a3
Sebastien Ahkrin <sebastien48criquet@gmail.com>
2020-06-29 14:47:30 +0200

    Updates

---
+++ precache-manifest.94105bc0ec429dd772a5ae8b4c18be6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1719ff7842dc2fff54404a96e733f9d",
+    "url": "/binary-clock/index.html"
+  },
+  {
+    "revision": "c9a70aa71378ddfa5b55",
+    "url": "/binary-clock/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "a8f9ff35ddcae26725da",
+    "url": "/binary-clock/static/js/2.43abacdb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/binary-clock/static/js/2.43abacdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9a70aa71378ddfa5b55",
+    "url": "/binary-clock/static/js/main.094d60d0.chunk.js"
+  },
+  {
+    "revision": "eccb8d4778412ca8b377",
+    "url": "/binary-clock/static/js/runtime-main.94552394.js"
+  }
+]);
