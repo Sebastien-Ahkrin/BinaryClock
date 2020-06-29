a041f7be5dc61633c50ae3c8a1c1f2144328579a
Sebastien Ahkrin <sebastien48criquet@gmail.com>
2020-06-29 15:09:40 +0200

    chore: install tailwind

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 export default function App () {
   return (
-    <div>
-      <p>Hello, World</p>
+    <div className="flex content-center flex-wrap h-screen justify-center">
+      <div className="w-1/3 p-2">
+        <div className="text-center text-6xl p-2">
+          <h1>Hello, World!</h1>
+        </div>
+      </div>
     </div>
   )
 }
